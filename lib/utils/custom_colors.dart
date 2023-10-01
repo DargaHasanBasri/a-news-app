b1c467632355c9ba99406b63800c0dd7781de566
dargahasanbasri <hasan.basri.darga@naylalabs.com>
2023-10-02 00:15:54 +0300

    Add shadow in custom button

--- lib/utils/custom_colors.dart
+++ lib/utils/custom_colors.dart
@@ -4,4 +4,5 @@ class CustomColors {
   static Color black = const Color(0xff000000);
   static Color white = const Color(0xffffffff);
   static Color customBottomNavigationColor = const Color(0xff7B53EC);
+  static Color niceColor = const Color(0xff007D8C);
 }
